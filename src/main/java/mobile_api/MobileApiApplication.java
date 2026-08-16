4a431167d32db5f3f90705afbcd07bc4fd3a08b5
nimesh <madurangan912@gmail.com>
2026-08-17 00:50:14 +0530

    Initial commit

---
+++ src/main/java/mobile_api/MobileApiApplication.java
@@ -0,0 +1,13 @@
+package mobile_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileApiApplication.class, args);
+	}
+
+}
